454735e632819b07716fcd669289f0592bdbd02e
Andrew0533 <56654631+Andrew0533@users.noreply.github.com>
2019-11-16 22:39:04 -0500

    Added Firing to the player

---
+++ Assets/Folder (Andrew)/Scripts (Andrew)/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Beloved, let us love one another: for love is of God; 
+// and every one that loveth is born of God, and knoweth God.
+// He that loveth not knoweth not God; for God is love. 
+// (1 John 4:7-8 KJV)
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+
+    Rigidbody2D rb;
+    
+    public float GetDamage()
+    {
+        Debug.Log("Getting Damage");
+        return damage;
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Folder (Andrew)/Scripts (Andrew)/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// There is no fear in love; but perfect love casteth out fear: 
+// because fear hath torment. He that feareth is not made perfect in love.
+// We love him, because he first loved us.
+// (1 John 4:18-19 KJV)
+
+public class Weapon : MonoBehaviour
+{
+    public Projectile proj;
+
+    public Projectile GetProjectile()
+    {
+        Debug.Log("Returning Projectile");
+        return proj;
+    }
+
+}
