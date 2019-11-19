440a8a1d75e018b3ce14e6dbe4f69dcaf15cf1ef
RyukeKitsune(Eric) <57182219+RyukeKitsune(Eric)@users.noreply.github.com>
2019-11-18 20:15:12 -0500

    Added Whitebox prototype scene with what we have so far. Fixed Cam too
    VCams will have to added individually with the trigger. Check My scene folder for prefabs and reference to settings.

---
+++ Assets/Folder (Eric Adkins)/Scripts/CameraTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine.Utility;
+using Cinemachine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public CinemachineVirtualCamera cameraList;
+    public CinemachineBrain mainCameraBrain;
+
+    private ICinemachineCamera currentCamera;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            currentCamera = mainCameraBrain.ActiveVirtualCamera;
+            if (currentCamera != cameraList)
+            {
+                currentCamera.VirtualCameraGameObject.SetActive(false);
+                cameraList.VirtualCameraGameObject.SetActive(true);
+            }
+        }
+    }
+}
